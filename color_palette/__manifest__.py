98643927b7c41283df65e7a33aeec640fed9e91f
Chau Le <chaulv@w360s.com>
2017-12-26 19:02:38 +0700

    First commit for color_palette for v10.0

--- color_palette/__manifest__.py
+++ color_palette/__manifest__.py
@@ -4,13 +4,13 @@
 {
     'license': 'LGPL-3',
     "name" : "Custom Color Palette",
-    "version" : "11.0",
+    "version" : "10.0",
     "author" : "W360S",
     'price': 9.90,
     'currency': 'EUR',
     "category" : "Extra Tools",
 	"summary": "Change the Odoo color palette made by (W360S)",
-    'description': "For Odoo Enterprise Version 11.0, this module replaces the default Apps Switcher background with a single gray image background. By replacing the image file in this module, you can further personalize the Odoo Apps Switcher page. This is a very simply module. It serves as an ideal example for those who are learning how to build modules with Odoo. or you want to change your own color just go to color_palette/static/src/less/variables.less and change these: @odoo-brand-primary: #0077cd; @odoo-brand-optional: #4d8496; @odoo-brand-secondary: #00ce2c; @odoo-brand-lightsecondary: #e2e2e0; as color you want.",
+    'description': "For Odoo Enterprise Version 10.0, this module replaces the default Apps Switcher background with a single gray image background. By replacing the image file in this module, you can further personalize the Odoo Apps Switcher page. This is a very simply module. It serves as an ideal example for those who are learning how to build modules with Odoo. or you want to change your own color just go to color_palette/static/src/less/variables.less and change these: @odoo-brand-primary: #0077cd; @odoo-brand-optional: #4d8496; @odoo-brand-secondary: #00ce2c; @odoo-brand-lightsecondary: #e2e2e0; as color you want.",
     'maintainer': "World 360° Services",
     'website': 'http://saastools.xyz',
     'images': ['static/description/banner.png'],
@@ -26,5 +26,4 @@
     "application": False,
     "installable": True,
 }
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
